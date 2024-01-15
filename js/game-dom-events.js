e8210378ea9a968111affe214ebebdd5596f3cd6
Amilcar Andr�s Miranda G�mez <AmilcarAndres.MirandaGomez@alumnos.uai.edu.ar>
2024-01-14 23:20:42 -0300

    Fucniones varias con .addEventListener en js

--- js/game-dom-events.js
+++ js/game-dom-events.js
@@ -31,3 +31,79 @@ var rankingNameButton = document.getElementById('ranking-name-title');
 var rankingLevelButton = document.getElementById('ranking-level-title');
 var rankingPointsButton = document.getElementById('ranking-points-title');
 var rankingDateButton = document.getElementById('ranking-date-title');
+
+nameInput.addEventListener('input', function () {
+    nameInput.style.width = this.value.length + 'ch';
+  });
+  
+  buttonStart.addEventListener('click', function () {
+    newGame();
+  });
+  
+  buttonGreen.addEventListener('click', function () {
+    if (gameStarted) {
+      colorClicked(GameColors.green);
+    }
+  });
+  
+  buttonRed.addEventListener('click', function () {
+    if (gameStarted) {
+      colorClicked(GameColors.red);
+    }
+  });
+  
+  buttonBlue.addEventListener('click', function () {
+    if (gameStarted) {
+      colorClicked(GameColors.blue);
+    }
+  });
+  
+  buttonYellow.addEventListener('click', function () {
+    if (gameStarted) {
+      colorClicked(GameColors.yellow);
+    }
+  });
+  
+  modalClose.addEventListener('click', function () {
+    closeModal();
+  });
+  
+  rankingClose.addEventListener('click', function () {
+    closeModal();
+  });
+  
+  buttonGithub.addEventListener('click', function () {
+    window.open(
+      'https://github.com/amilcarmiranda/SIMON-LPPA',
+      '_blank'
+    );
+  });
+  
+  buttonContact.addEventListener('click', function () {
+    window.location.href = 'contact.html';
+  });
+  
+  buttonScore.addEventListener('click', function () {
+    showRanking(RankingOrder.points);
+  });
+  
+  rankingPositionButton.addEventListener('click', function () {
+    showRanking(RankingOrder.points);
+  });
+  
+  rankingNameButton.addEventListener('click', function () {
+    showRanking(RankingOrder.name);
+  });
+  
+  rankingLevelButton.addEventListener('click', function () {
+    showRanking(RankingOrder.level);
+  });
+  
+  rankingPointsButton.addEventListener('click', function () {
+    showRanking(RankingOrder.points);
+  });
+  
+  rankingDateButton.addEventListener('click', function () {
+    showRanking(RankingOrder.date);
+  });
+  
