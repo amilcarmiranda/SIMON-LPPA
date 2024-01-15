4e10a8a38ba3b2ac7a674cd4dbc311db44319425
Amilcar Andr�s Miranda G�mez <AmilcarAndres.MirandaGomez@alumnos.uai.edu.ar>
2024-01-14 22:16:24 -0300

    var (s) almacenan el.. DOM

---
+++ js/game-dom-events.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var buttonStart = document.getElementById('start-button');
+
+var buttonGreen = document.getElementById('green');
+var buttonRed = document.getElementById('red');
+var buttonBlue = document.getElementById('blue');
+var buttonYellow = document.getElementById('yellow');
+
+var labelLevel = document.getElementById('level');
+var labelScore = document.getElementById('score');
+var labelTimer = document.getElementById('timer');
+var labelState = document.getElementById('state');
+
+var nameInput = document.getElementById('input-name');
+
+var modal = document.getElementById('modal');
+var modalMessage = document.getElementById('modal-message');
+var modalClose = document.getElementById('modal-close');
+
+var buttonGithub = document.getElementById('button-github');
+var buttonScore = document.getElementById('button-score');
+var buttonContact = document.getElementById('button-contact');
+
+var ranking = document.getElementById('ranking');
+var rankingList = document.getElementById('ranking-list');
+var rankingClose = document.getElementById('ranking-close');
+
+var rankingPositionButton = document.getElementById('ranking-position-title');
+var rankingNameButton = document.getElementById('ranking-name-title');
+var rankingLevelButton = document.getElementById('ranking-level-title');
+var rankingPointsButton = document.getElementById('ranking-points-title');
+var rankingDateButton = document.getElementById('ranking-date-title');
